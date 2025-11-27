3ad2f579ba735cb783cdcaa5c2730e430daad2f6
Miqueias <miqueias.rihs@gmail.com>
2025-11-27 16:08:52 -0300

    fix

--- core/factories.py
+++ core/factories.py
@@ -8,6 +8,7 @@ import factory
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
+        skip_postgeneration_save = True
 
     username = factory.Sequence(lambda n: f"producer_user_{n}")
     email = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
